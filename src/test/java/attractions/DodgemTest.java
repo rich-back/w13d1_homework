f021f145875472a4320ba279bd12d6585a76f0cd
rich-back <backhouse.richard@gmail.com>
2023-01-23 18:46:08 +0000

    implemented ITicketed to Dodgems and tested

--- src/test/java/attractions/DodgemTest.java
+++ src/test/java/attractions/DodgemTest.java
@@ -2,16 +2,22 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class DodgemTest {
 
     Dodgems dodgems;
+    Visitor visitorUnder12;
+    Visitor visitorOver12;
 
     @Before
     public void setUp() throws Exception {
         dodgems = new Dodgems("Bumper Cars", 5);
+        visitorUnder12 = new Visitor(11, 1.5, 20);
+        visitorOver12 = new Visitor(13, 1.4, 200);
+
     }
 
 
@@ -28,5 +34,14 @@ public class DodgemTest {
     @Test
     public void hasVisitCount() {
         assertEquals(0, dodgems.getVisitCount());
+    }
+    @Test
+    public void willChargeDefaultIfOver12() {
+        assertEquals(4.50, dodgems.priceFor(visitorOver12), 0.0);
+    }
+
+    @Test
+    public void willChargeHalfIfUnder12() {
+        assertEquals(2.25, dodgems.priceFor(visitorUnder12), 0.0);
     }
 }
